7b5d1974ce0428e950c27faa9935922070d9e4de
theharshon <harshkumarecom@gmail.com>
2024-02-23 15:48:28 +0530

    init char+ 2nd level

--- lib/pear_adventure.dart
+++ lib/pear_adventure.dart
@@ -10,10 +10,13 @@ class PearAdventure extends FlameGame {
   Color backgroundColor() => const Color(0xFF211F30);
   late final CameraComponent cam;
 
-  final world = Level();
+  final world = Level(levelName: 'Level-02');
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    //load all images into cache
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
         world: world, width: 640, height: 360);
     cam.viewfinder.anchor = Anchor.topLeft;
